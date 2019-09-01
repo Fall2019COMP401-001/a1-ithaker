6df9a4b53fed221479fb5b57c5f678f6f2056533
Ishan <Ishan@192.168.1.86>
2019-09-01 17:31:45 -0400

    attempt 1, one person

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,35 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int customer = scan.nextInt();
+		// number of customers
+		int[] values = new int[customer];
+		
+		var firstname = scan.next();
+		var lastname = scan.next();
+		int items = scan.nextInt();
+		
+		for (int i=0; i<items; i++) {
+			values[i] = scan.nextInt();
+		}
+		
+		scan.close();
+		
+		
+		int sum = calculateValueSum(values);
+
+		System.out.println(firstname + ". " + lastname.charAt(0) + ": " + sum);
+
+	
+	}	
+static int calculateValueSum(int[] vals) {
+		
+		int sum = 0;
+		
+		for (int i=0; i<vals.length; i++) {
+			sum += vals[i];
+		}
+		
+		return sum;
 	}
 }
